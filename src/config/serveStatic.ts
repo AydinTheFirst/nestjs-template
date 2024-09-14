c2a920a53c3db777d6983f10f7a67228ab396410
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-09-14 23:03:51 +0300

    Refactor serveStaticConfig to use relative path

--- src/config/serveStatic.ts
+++ src/config/serveStatic.ts
@@ -1,17 +1,7 @@
 import { ServeStaticModuleOptions } from "@nestjs/serve-static";
 import { join } from "path";
 
-const clientDist = join(
-  __dirname,
-  "..",
-  "..",
-  "..",
-  "..",
-  "..",
-  "apps",
-  "client",
-  "dist"
-);
+const clientDist = join(process.cwd(), "..", "apps", "client", "dist");
 
 export const serveStaticConfig: ServeStaticModuleOptions = {
   rootPath: clientDist,
