552f2ace0b50c2c7bfedf4f91f83afb14e7d0865
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-01-30 14:20:36 +0300

    Revert "Add channels and messages modules with DTOs, controllers, and services"
    This reverts commit 4e4627d38d5f53a7686a37c7562a4b8e683033d7.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,4 @@
 import { AuthModule } from "./auth";
-import { ChannelsModule } from "./channels/channels.module";
-import { MessagesModule } from "./messages/messages.module";
 import { UsersModule } from "./users";
 
-export const AppRoutes = [
-  AuthModule,
-  UsersModule,
-  MessagesModule,
-  ChannelsModule,
-];
+export const AppRoutes = [AuthModule, UsersModule];
--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,8 +1,6 @@
 import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server, Socket } from "socket.io";
-
-import { PrismaService, User } from "@/prisma";
+import { Server } from "socket.io";
 
 @Injectable()
 @WebSocketGateway({
@@ -12,58 +10,15 @@ import { PrismaService, User } from "@/prisma";
   transports: ["websocket"],
 })
 export class WebsocketGateway implements OnModuleInit {
-  private connectedSockets = new Map<string, { socket: Socket; user: User }>();
+  private connectedSockets = new Map<string, any>();
+
   @WebSocketServer()
   io: Server;
 
-  constructor(private prisma: PrismaService) {}
-
-  async authenticate(socket: Socket) {
-    const token = socket.handshake.auth.token;
-
-    if (!token) {
-      this.kickSocket(socket.id, "Unauthorized");
-    }
-
-    const tokenDoc = await this.prisma.token.findUnique({
-      include: {
-        user: true,
-      },
-      where: {
-        token,
-      },
-    });
-
-    if (!tokenDoc) {
-      this.kickSocket(socket.id, "Unauthorized");
-    }
-
-    if (tokenDoc.expiresAt < new Date()) {
-      this.kickSocket(socket.id, "Token expired");
-    }
-
-    this.connectedSockets.set(socket.id, { socket, user: tokenDoc.user });
-  }
-
-  async kickSocket(socketId: string, reason: string) {
-    if (!this.connectedSockets.has(socketId)) {
-      return;
-    }
-
-    const { socket } = this.connectedSockets.get(socketId);
-
-    socket.emit("error", reason);
-    socket.disconnect();
-  }
-
   onModuleInit() {
-    this.io.on("connection", async (socket) => {
-      await this.authenticate(socket);
+    this.io.on("connection", (socket) => {
       Logger.debug(`Client connected: ${socket.id}`, "WebsocketGateway");
-
-      socket.on("join", async (channelId: string) => {
-        socket.join(channelId);
-      });
+      this.connectedSockets.set(socket.id, socket);
 
       socket.on("disconnect", () => {
         this.connectedSockets.delete(socket.id);
